f6decd5db68295e3bca14e785cd8dd0bc0d21cf5
kartikey <p.kartikey.p@gmail.com>
2025-06-23 13:08:45 +0530

    Kartikey Add Online_Quiz_Application

--- Online_Quiz_Application.java
+++ Online_Quiz_Application.java
@@ -3,16 +3,10 @@ public class Online_Quiz_Application {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String[] qs = {
-				
-				
 				"1. Java is a?\nA. Platform-independent\nB. OOP\nC. Secure\nD. All",
-	             
 				"2. Size of int in Java?\nA. 2 bytes\nB. 4 bytes\nC. 8 bytes\nD. OS dependent",
-	             
 	            "3. Keyword to inherit class?\nA. implement\nB. super\nC. extends\nD. this",
-	             	                         
 	            "4. JVM stands for?\nA. Java Virtual Machine\nB. Java Verified Mode\nC. Java Variable Method\nD. None",
-	             
 	            "5. Fixed loop usage?\nA. while\nB. do-while\nC. for\nD. if"
 	         };
 
